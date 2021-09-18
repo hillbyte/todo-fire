8fdcb8269be6aef4b68cc8c46cab03682cc35558
hillbyte <ahillbaky@gmail.com>
2021-09-19 00:21:59 +0530

    remote git

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDP_TfEKRpx7mZXsbscnr1qwwrelsROb8Q",
-  authDomain: "notes-92260.firebaseapp.com",
-  projectId: "notes-92260",
-  storageBucket: "notes-92260.appspot.com",
-  messagingSenderId: "898287426224",
-  appId: "1:898287426224:web:7451b26529b54116d3b975",
+  
 };
 
 firebase.initializeApp(firebaseConfig);
